8795fe2f9d925ddd2fcca6316225f003a60363f7
Bill <19377612+billfasoli@users.noreply.github.com>
2022-10-28 21:52:22 -0700

    Update mate-panel-vpnip.sh

--- mate-panel-vpnip.sh
+++ mate-panel-vpnip.sh
@@ -1,3 +1,6 @@
+# script that shows your IP when connected to a VPN
+# to get this to run on Parrot OS, will need to turn into a binary
+
 !/bin/sh
 
 ip="$(ip a s tun0 2>/dev/null | grep -o -P '(?<=inet )[0-9]{1,3}(\.[0-9]{1,3}){3}')"
